e7669eea81782da59c3bda79b8c2d6855d443ede
siddhikotre <siddhikotre72@gmail.com>
2023-01-22 22:03:21 +0530

    22/1/23 commit4

--- bearsandbulls/bnb/urls.py
+++ bearsandbulls/bnb/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('login/', views.login, name="login"),
     path('register/', views.register, name="register"),
+    path('login/', views.login, name="login"),
     path('rules/', views.rules, name="rules"),
     path('pregame/', views.pregame, name="pregame"),
     path('game/', views.game, name="game"),
--- bearsandbulls/bnb/views.py
+++ bearsandbulls/bnb/views.py
@@ -1,18 +1,42 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse('Hey your app work thank god')
-
-
-def login(request):
-    return render(request, 'login.html')
+    return HttpResponse('Hey thank god your app is working')
 
 
 def register(request):
+    print("Inside register")
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = User.objects.create_user(username=username, email=email, password=password, )
+        user.save();
+        messages.success(request, "Your account has been successfully created")
+    return render(request, 'register.html')
+
+
+def login(request):
+    print("Inside login")
+    if request.method == 'POST':
+        username1 = request.POST['username1']
+        password1 = request.POST['password1']
+        print(f"username1{username1} password1{password1}")
+        user1 = authenticate(username=username1, password=password1)
+
+        if user1 is not None:
+            return render(request, "rules.html")
+        else:
+            return render(request, "register.html", {"error": "Invalid username or password."})
     return render(request, 'register.html')
 
 
